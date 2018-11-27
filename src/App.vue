cd17ece4a5b7c60c67df40b3a9d06a83580346ea
BANI Zakaria <zakariabani@gmail.com>
2018-11-27 23:46:18 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <ProductList
+      :products="products"
+      header="Products List"
+      @remove="remove"
+    />
+  </div>
+</template>
+
+<script>
+import ProductList from './components/ProductList.vue'
+import axios from 'axios'
+
+export default {
+  name: 'app',
+  components: {
+    ProductList
+  },
+  data() {
+    return {
+      products: []
+    }
+  },
+  created: function () {
+    this.fetchProducts();
+  },
+  methods: {
+    fetchProducts: function() {
+      axios.get('./api/products.json')
+        .then(res => this.products = res.data)
+    },
+    remove: function (index) {
+      this.products.splice(index, 1);            
+    }
+  }
+}
+</script>
+
+<style>
+  [v-cloak] {
+    display: none;
+  }
+</style>
